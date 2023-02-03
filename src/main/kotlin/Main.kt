998b9abf95857e531046bcf71ee8802efda96c2a
Angeth-Herjok <angethherjok@gmail.com>
2023-02-03 16:17:25 +0300

    mach

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    var name = "mach"
+    var weight = "45"
+    var citizenship = "kenyan"
+    var phonenumber = "079049578870"
+    var age = "9"
+    println(name)
+
+}
